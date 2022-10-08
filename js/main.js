a3cb6da752978fa810abf8f969c6725b6f50e5e0
Matías Bidarte <matias.bidarte@outlook.com>
2022-10-08 20:50:24 -0300

    css terminado

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// MENU HAMBURGESA
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header__content__navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".header__content__navbar-item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+// APP
