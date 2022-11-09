5b734823f8c07b0de02dd3c4c0c95b0c66fd2d7e
Matías Bidarte <matias.bidarte@outlook.com>
2022-11-09 15:44:00 -0300

    orden visual

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // =============== HAMBURGUER MENU ===============
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".header__content__navbar");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".header__content__navbar");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-document.querySelectorAll(".header__content__navbar-item").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+// document.querySelectorAll(".header__content__navbar-item").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }))
 
 
 // =============== APP ===============
